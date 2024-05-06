f1c12b39838fd16034444767b38c0dff1bc57dd5
Anderseon200 <anderson.barreto01@uptc.edu.co>
2024-05-05 23:17:04 -0500

    update security

--- src/main/java/edu/uptc/swii/shiftmgmt/config/SecurityConfig.java
+++ src/main/java/edu/uptc/swii/shiftmgmt/config/SecurityConfig.java
@@ -1,25 +1,36 @@
 package edu.uptc.swii.shiftmgmt.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwkSetUri;
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(http -> http.anyRequest().authenticated())
-            .oauth2ResourceServer(oauth ->{
-                oauth.jwt(jwt -> {});
-            })
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .build();
+                .csrf(csrf -> csrf.disable())
+                .authorizeRequests(http -> http.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth ->{
+                    oauth.jwt(jwt -> {});
+                })
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
     }
 }
