b626c84905c2365b13782d930d99c2bfd34d87e2
alexanderr4 <jose231101a@gmail.com>
2024-05-14 16:09:02 -0500

    ApiGateway funcional

--- apiGateway/src/main/java/edu/uptc/swii/apiGateway/ApiGatewayApplication.java
+++ apiGateway/src/main/java/edu/uptc/swii/apiGateway/ApiGatewayApplication.java
@@ -3,6 +3,7 @@ package edu.uptc.swii.apiGateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ApiGatewayApplication {
 
