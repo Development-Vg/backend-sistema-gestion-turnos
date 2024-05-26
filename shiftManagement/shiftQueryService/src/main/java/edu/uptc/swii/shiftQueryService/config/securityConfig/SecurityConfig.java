e724d589f07ea7103608d36183588f54f8f14c29
alexanderr4 <jose231101a@gmail.com>
2024-05-26 14:04:06 -0500

    toke para todas la peticiones

---
+++ shiftManagement/shiftQueryService/src/main/java/edu/uptc/swii/shiftQueryService/config/securityConfig/SecurityConfig.java
@@ -0,0 +1,43 @@
+package edu.uptc.swii.shiftCommandService.config.securityConfig;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Autowired
+    private JwtAuthenticationConverter jwtAuthenticationConverter;
+//    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+//    private String jwkSetUri;
+
+//    @Bean
+//    public JwtDecoder jwtDecoder() {
+//        return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
+//    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeRequests(http -> http
+                        .requestMatchers("/listUsers/listAll").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth ->{
+                    oauth.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter));
+                })
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+
+}
