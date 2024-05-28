4cb9fa6dc529c7edb9833f6f4a03ba2489399bdd
alexanderr4 <jose231101a@gmail.com>
2024-05-27 23:06:13 -0500

    registar usuario

--- manageUsers/UserCommandService/src/main/java/edu/uptc/swii/UserCommandService/domain/repository/CredentialRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.uptc.swii.UserCommandService.domain.repository;
-
-import edu.uptc.swii.UserCommandService.domain.model.Credentials;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CredentialRepository extends JpaRepository<Credentials, Integer>{
-    
-}
--- manageUsers/UserQueryService/src/main/java/edu/uptc/swii/UserQueryService/domain/repository/CredentialRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.uptc.swii.UserQueryService.domain.repository;
-
-import edu.uptc.swii.UserQueryService.domain.model.Credentials;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CredentialRepository extends JpaRepository<Credentials, Integer>{
-    
-}
